12ea01994534d96c4e81846e069789e0f7413d4f
Riggs <riggs9162@gmx.de>
2024-12-26 05:45:31 +0100

    Update sh_module.lua

--- lua/vanguard/modules/weapons/sh_module.lua
+++ lua/vanguard/modules/weapons/sh_module.lua
@@ -4,7 +4,7 @@ MODULE.Name = "Weapons"
 MODULE.Description = "A base module for creating simple weapons, very similar to the Longsword Weapon Base."
 MODULE.Author = "Riggs"
 MODULE.Version = "0.5.0"
-MODULE.VersionLink = "https://raw.githubusercontent.com/Minerva-Servers/vwep/refs/heads/main/VERSION.txt"
+MODULE.VersionLink = "https://raw.githubusercontent.com/Minerva-Servers/vanguard-weapons/refs/heads/main/VERSION.txt"
 
 vanguard.util:Include("sh_commands.lua")
 vanguard.util:Include("sh_firemodes.lua")
